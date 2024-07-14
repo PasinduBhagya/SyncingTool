5823914ab1d3ebf9a3bd97aa9cebaa1ff19b13bd
PasinduBhagya <pasindubhagyapeiries@gmail.com>
2024-07-14 18:48:07 +0530

    Sun Jul 14 06:48:07 PM +0530 2024

---
+++ ExecuteLinuxCommand.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ExecuteLinuxCommand {
+    public static void main(String[] args) {
+        String command = "ls -l"; // Example command
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+            reader.close();
+
+            int exitCode = process.waitFor();
+            System.out.println("Exited with code: " + exitCode);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
